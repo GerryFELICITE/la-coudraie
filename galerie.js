fbec25cb45a637ec5b1af2d91eec186c3e0d13bd
Marc-Antoine Letort <marcantoine.letort@gmail.com>
2025-04-09 17:38:26 +0200

    Ajout des vraies images de la galerie

--- galerie.js
+++ galerie.js
@@ -12,13 +12,42 @@ document.addEventListener("DOMContentLoaded", () => {
     let currentIndex = 0; // pour savoir quelle image est affichée
   
     const photos = [
-        { src: "assests/images/galerie/chambre1/20190114_100129 (1).jpg", category: "cuisine" },
-        { src: "assests/images/galerie/cuisine/20190114_104353 (1).jpg", category: "salon" },
-        { src: "assests/images/galerie/chambre1/20190114_100129 (1).jpg", category: "chambre1" },
-        { src: "assests/images/galerie/chambre1/20190114_100129 (1).jpg", category: "salle-de-bain" },
-        { src: "assests/images/galerie/cuisine/20190114_104353 (1).jpg", category: "piscine" },
-        { src: "assests/images/galerie/cuisine/20190114_104353 (1).jpg", category: "cuisine" },
-        { src: "assests/images/galerie/chambre1/20190114_100129 (1).jpg", category: "piscine" },
+        { thumb: "assests/images/galerie/salon/salon1.webp", full: "assests/images/galerie/salon/salon1.jpg", category: "salon" },
+        { thumb: "assests/images/galerie/salon/salon2.webp", full: "assests/images/galerie/salon/salon2.jpg", category: "salon" },
+        { thumb: "assests/images/galerie/salon/salon3.webp", full: "assests/images/galerie/salon/salon3.jpg", category: "salon" },
+        { thumb: "assests/images/galerie/salon/salon4.webp", full: "assests/images/galerie/salon/salon4.jpg", category: "salon" },
+        { thumb: "assests/images/galerie/cuisine/cuisine1.webp", full: "assests/images/galerie/cuisine/cuisine1.jpg", category: "cuisine" },
+        { thumb: "assests/images/galerie/cuisine/cuisine2.webp", full: "assests/images/galerie/cuisine/cuisine2.jpg", category: "cuisine" },
+        { thumb: "assests/images/galerie/piscine/piscine1.webp", full: "assests/images/galerie/piscine/piscine1.jpg", category: "piscine" },
+        { thumb: "assests/images/galerie/piscine/piscine2.webp", full: "assests/images/galerie/piscine/piscine2.jpg", category: "piscine" },
+        { thumb: "assests/images/galerie/piscine/piscine3.webp", full: "assests/images/galerie/piscine/piscine3.jpg", category: "piscine" },
+        { thumb: "assests/images/galerie/chambreRomantique/chambreRomantique1.webp", full: "assests/images/galerie/chambreRomantique/chambreRomantique1.jpg", category: "chambreRomantique" },
+        { thumb: "assests/images/galerie/chambreRomantique/chambreRomantique2.webp", full: "assests/images/galerie/chambreRomantique/chambreRomantique2.jpg", category: "chambreRomantique" },
+        { thumb: "assests/images/galerie/chambreRomantique/chambreRomantique3.webp", full: "assests/images/galerie/chambreRomantique/chambreRomantique3.jpg", category: "chambreRomantique" },
+        { thumb: "assests/images/galerie/chambreRomantique/chambreRomantique4.webp", full: "assests/images/galerie/chambreRomantique/chambreRomantique4.jpg", category: "chambreRomantique" },
+        { thumb: "assests/images/galerie/chambreJungle/chambreJungle1.webp", full: "assests/images/galerie/chambreJungle/chambreJungle1.jpg", category: "chambreJungle" },
+        { thumb: "assests/images/galerie/chambreJungle/chambreJungle2.webp", full: "assests/images/galerie/chambreJungle/chambreJungle2.jpg", category: "chambreJungle" },
+        { thumb: "assests/images/galerie/chambreMer/chambreMer1.webp", full: "assests/images/galerie/chambreMer/chambreMer1.jpg", category: "chambreMer" },
+        { thumb: "assests/images/galerie/chambreMer/chambreMer2.webp", full: "assests/images/galerie/chambreMer/chambreMer2.jpg", category: "chambreMer" },
+        { thumb: "assests/images/galerie/chambreMer/chambreMer3.webp", full: "assests/images/galerie/chambreMer/chambreMer3.jpg", category: "chambreMer" },
+        { thumb: "assests/images/galerie/chambreMer/chambreMer4.webp", full: "assests/images/galerie/chambreMer/chambreMer4.jpg", category: "chambreMer" },
+        { thumb: "assests/images/galerie/sdbMer/sdbMer1.webp", full: "assests/images/galerie/sdbMer/sdbMer1.jpg", category: "sdbMer" },
+        { thumb: "assests/images/galerie/sdbRomantique/sdbRomantique1.webp", full: "assests/images/galerie/sdbRomantique/sdbRomantique1.jpg", category: "sdbRomantique" },
+        { thumb: "assests/images/galerie/sdbRomantique/sdbRomantique2.webp", full: "assests/images/galerie/sdbRomantique/sdbRomantique2.jpg", category: "sdbRomantique" },
+        { thumb: "assests/images/galerie/sdj/sdj1.webp", full: "assests/images/galerie/sdj/sdj1.jpg", category: "sdj" },
+        { thumb: "assests/images/galerie/sdj/sdj2.webp", full: "assests/images/galerie/sdj/sdj2.jpg", category: "sdj" },
+        { thumb: "assests/images/galerie/veranda/veranda1.webp", full: "assests/images/galerie/veranda/veranda1.jpg", category: "veranda" },
+        { thumb: "assests/images/galerie/veranda/veranda2.webp", full: "assests/images/galerie/veranda/veranda2.jpg", category: "veranda" },
+        { thumb: "assests/images/galerie/veranda/veranda3.webp", full: "assests/images/galerie/veranda/veranda3.jpg", category: "veranda" },
+        { thumb: "assests/images/galerie/veranda/veranda4.webp", full: "assests/images/galerie/veranda/veranda4.jpg", category: "veranda" },
+        { thumb: "assests/images/galerie/veranda/veranda5.webp", full: "assests/images/galerie/veranda/veranda5.jpg", category: "veranda" },
+        { thumb: "assests/images/galerie/jardin/jardin1.webp", full: "assests/images/galerie/jardin/jardin1.jpg", category: "jardin" },
+        { thumb: "assests/images/galerie/jardin/jardin2.webp", full: "assests/images/galerie/jardin/jardin2.jpg", category: "jardin" },
+        { thumb: "assests/images/galerie/jardin/jardin3.webp", full: "assests/images/galerie/jardin/jardin3.jpg", category: "jardin" },
+        { thumb: "assests/images/galerie/jardin/jardin4.webp", full: "assests/images/galerie/jardin/jardin4.jpg", category: "jardin" },
+        { thumb: "assests/images/galerie/jardin/jardin5.webp", full: "assests/images/galerie/jardin/jardin5.jpg", category: "jardin" },
+        { thumb: "assests/images/galerie/jardin/jardin6.webp", full: "assests/images/galerie/jardin/jardin6.jpg", category: "jardin" },
+        { thumb: "assests/images/galerie/jardin/jardin7.webp", full: "assests/images/galerie/jardin/jardin7.jpg", category: "jardin" }
       ];
   
       let currentPhotos = []; // tableau filtré global
@@ -29,14 +58,14 @@ document.addEventListener("DOMContentLoaded", () => {
       
         currentPhotos.forEach((p, index) => {
           const img = document.createElement("img");
-          img.src = p.src;
+          img.src = p.thumb;
           img.alt = p.category;
           img.dataset.category = p.category;
           gallery.appendChild(img);
       
           img.addEventListener("click", () => {
             lightbox.classList.add("active");
-            lightboxImg.src = p.src;
+            lightboxImg.src = p.full;
             lightboxImg.alt = p.category;
             currentIndex = index;
           });
@@ -66,14 +95,14 @@ document.addEventListener("DOMContentLoaded", () => {
     lightboxNext.addEventListener("click", () => {
         currentIndex = (currentIndex + 1) % currentPhotos.length;
         const next = currentPhotos[currentIndex];
-        lightboxImg.src = next.src;
+        lightboxImg.src = next.full;
         lightboxImg.alt = next.category;
       });
       
       lightboxPrev.addEventListener("click", () => {
         currentIndex = (currentIndex - 1 + currentPhotos.length) % currentPhotos.length;
         const prev = currentPhotos[currentIndex];
-        lightboxImg.src = prev.src;
+        lightboxImg.src = prev.full;
         lightboxImg.alt = prev.category;
       });
       
